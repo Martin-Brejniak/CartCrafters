33af84aea5a2248cdfa39da80585032f9d31b966
Hartley-Madison-215062417 <maddi15@my.yorku.ca>
2023-11-10 22:03:35 -0500

    Forgot some stuff

---
+++ CartCrafters/src/main/java/com/checkout/CheckOut.java
@@ -0,0 +1,14 @@
+package com.checkout;
+
+public class CheckOut {
+
+	public CheckOut() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
