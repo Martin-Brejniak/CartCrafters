be836497d6746ca5b70fc580d68ee4f604a6894e
Martin-Brejniak <77299294+Martin-Brejniak@users.noreply.github.com>
2023-11-29 00:10:03 -0500

    User Module nearly completed

---
+++ UserServer/src/main/java/com/example/UserServer/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.example.UserServer.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.UserServer.entity.User;
+import com.example.UserServer.service.UserDAO;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/user")
+public class UserController {
+	
+private final UserDAO userDAO;
+	
+	@Autowired
+	public UserController(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+	
+	@GetMapping("/get-all")
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+	
+	@GetMapping("/get-id")
+    public List<User> getUserByID(@RequestParam("userID") int userID) {
+		return userDAO.getUserByID(userID);
+    }
+	
+	@GetMapping("/get-username")
+    public List<User> getUserByID(@RequestParam("username") String username) {
+		return userDAO.getUserByUsername(username);
+    }
+	
+	@GetMapping("/authenticate")
+    public boolean authenticateUser(@RequestParam("username") String username, @RequestParam("password") String password) {
+		return userDAO.authenticateUser(username, password);
+    }
+	
+	@PostMapping
+    public void createUser(@RequestBody User user) {
+		userDAO.createUser(user);
+    }
+	
+	@PutMapping
+    public void updateUser(@RequestBody User user, @RequestParam("userID") int userID) {
+		userDAO.updateUser(userID, user);
+    }
+	
+	@DeleteMapping
+    public void deleteUser(@RequestParam("userID") int userID) {
+		userDAO.deleteUser(userID);
+    }
+
+}
---
+++ UserServer/src/main/java/com/example/UserServer/service/UserDAO.java
@@ -0,0 +1,130 @@
+package com.example.UserServer.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.example.UserServer.entity.User;
+
+@Repository
+public class UserDAO {
+	
+	public JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public UserDAO(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	private RowMapper<User> userRowMapper = (rs, rowNum) -> {
+		User user = new User();
+		user.setfName(rs.getString("fName"));
+		user.setlName(rs.getString("lName"));
+		user.setAddress(rs.getString("address"));
+		user.setPostal(rs.getString("postal"));
+		user.setCity(rs.getString("city"));
+		user.setCountry(rs.getString("country"));
+		user.setProvince(rs.getString("province"));
+		user.setUsername(rs.getString("username"));
+		user.setUserID(rs.getInt("userID"));
+		return user;
+	};
+	
+	private RowMapper<String> passwordRowMapper = (rs, rowNum) -> {
+		String password = rs.getString("password");
+		return password;
+	};
+	
+	/**
+	* Gets all items from the items database.
+	*
+	* @return         all items.
+	*/
+	public List<User> getAllUsers() {
+		String sql = "SELECT * FROM users";
+		return jdbcTemplate.query(sql, userRowMapper);
+	}
+	
+	/**
+	* Gets user with a matching ID from the users database.
+	*
+	* @param  id   a number.
+	* @return         user.
+	*/
+	public List<User> getUserByID(int id) {
+		String sql = "SELECT * FROM users WHERE userID = " + id;
+		return jdbcTemplate.query(sql, userRowMapper);
+	}
+		
+	/**
+	* Gets user with a matching username from the users database.
+	*
+	* @param  id   a username.
+	* @return         user.
+	*/
+	public List<User> getUserByUsername(String username) {
+		String sql = "SELECT * FROM users WHERE username = '" + username +"'";
+		return jdbcTemplate.query(sql, userRowMapper);
+	}	
+	
+	/**
+	* Checks to see if the username and password the user typed in, matches with what's in the database.
+	*
+	* @param  username   a username.
+	* @param  password   a password.
+	* @return         true if username and password are correct. False if not.
+	*/
+	public boolean authenticateUser(String username, String password) {
+		String sql = "SELECT password FROM users WHERE username = '" + username +"'";
+		String passwordDB = null;
+		boolean correct = false;
+		
+		passwordDB = jdbcTemplate.query(sql, passwordRowMapper).get(0);
+		
+		if (password.equals(passwordDB) ) {
+			correct = true;
+		}
+		return correct;
+	}
+	
+    /**
+    * Add user to user database.
+    *
+    * @param  user   a user.
+    */
+	public void createUser(User user) {
+		String sql = "INSERT INTO users(fname, lname, address, postal, city, country, province, username, password) VALUES('"+user.getfName()+"','"+user.getlName()+"','"+user.getAddress()+"','"+user.getPostal()+"','"+user.getCity()+"','"+user.getCountry()+"','"+user.getProvince()+"','"+user.getUsername()+"','"+user.getPassword()+"')";
+		jdbcTemplate.update(sql);
+	}
+
+	/**
+	* Updates user in user database.
+	*
+    * @param  id   a number.
+	* @param  user   a user.
+	*/
+	public void updateUser(int id, User user) {
+		//use prepared statements
+		String sql = "UPDATE users SET fname = '"+user.getfName()+"', lname = '"+user.getlName()+"', address = '"+user.getAddress()+"', postal = '"+user.getPostal()+"', city = '"+user.getCity()+"', country = '"+user.getCountry()+"', province = '"+user.getProvince()+"', username = '"+user.getUsername()+"', password = '"+user.getPassword()+"' WHERE userID = "+id;
+		jdbcTemplate.update(sql);
+	}
+	
+	/**
+	* Deletes user in user database.
+	*
+    * @param  id   a number.
+	*/
+	public void deleteUser(int id) {
+		String sql = "DELETE FROM users WHERE userID = "+ id;
+		jdbcTemplate.update(sql);
+	}
+	
+
+}
