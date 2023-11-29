c618989dd3a14e9e38992f993ad0cbdac8db5197
Martin-Brejniak <77299294+Martin-Brejniak@users.noreply.github.com>
2023-11-29 09:43:32 -0500

    Add files via upload

---
+++ UserServer/src/main/java/com/example/UserServer/controller/UserViewController.java
@@ -0,0 +1,18 @@
+package com.example.UserServer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserViewController {
+	@GetMapping("/signin-user")
+    public String signInPage() {
+        return "signin"; 
+    }
+    
+    @GetMapping("/signup-user")
+    public String signUpPage() {
+        return "signup";
+    }
+}
+
