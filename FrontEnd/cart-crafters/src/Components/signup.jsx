5468b125b74b3935257f3847dbc27d8f45ee9d44
Anika Prova <anika98@my.yorku.ca>
2023-12-06 15:08:43 -0500

    latest push

--- FrontEnd/cart-crafters/src/Components/signup.jsx
+++ FrontEnd/cart-crafters/src/Components/signup.jsx
@@ -38,8 +38,17 @@ function SignUp() {
     <div style={{ textAlign: 'center' }}>
       <h2>Sign Up</h2>
       {showWelcome && <p>Welcome! You will be redirected shortly.</p>}
+      {showWelcome && <p>Welcome! You will be redirected shortly.</p>}
       <form onSubmit={handleSubmit}>
-        {/* Form fields as before */}
+        <input type="text" name="fName" value={userData.fName} onChange={handleChange} placeholder="First Name" /><br/>
+        <input type="text" name="lName" value={userData.lName} onChange={handleChange} placeholder="Last Name" /><br/>
+        <input type="text" name="address" value={userData.address} onChange={handleChange} placeholder="Address" /><br/>
+        <input type="text" name="postal" value={userData.postal} onChange={handleChange} placeholder="Postal Code" /><br/>
+        <input type="text" name="city" value={userData.city} onChange={handleChange} placeholder="City" /><br/>
+        <input type="text" name="country" value={userData.country} onChange={handleChange} placeholder="Country" /><br/>
+        <input type="text" name="province" value={userData.province} onChange={handleChange} placeholder="Province" /><br/>
+        <input type="text" name="username" value={userData.username} onChange={handleChange} placeholder="Username" /><br/>
+        <input type="password" name="password" value={userData.password} onChange={handleChange} placeholder="Password" /><br/>
         <button type="submit">Sign Up</button>
       </form>
     </div>
